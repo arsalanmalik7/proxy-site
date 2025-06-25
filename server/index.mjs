7df532696833834902383b3d81c3eadab9185cb2
marianoor09 <marianoor09@outlook.com>
2025-06-25 23:35:51 +0500

    headers changed

--- server/index.mjs
+++ server/index.mjs
@@ -26,7 +26,19 @@ app.get('/proxy-iframe-content', async (req, res) => {
             console.log('DNS lookup:', err || address);
         });
 
-        const response = await axios.get(targetUrl);
+        const response = await axios.get(targetUrl, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Referer': 'https://www.google.com/',
+                'Connection': 'keep-alive',
+                'Upgrade-Insecure-Requests': '1',
+                'Cache-Control': 'no-cache'
+            },
+            timeout: 10000,
+        });
+
 
         res.send(response.data);
     } catch (error) {
